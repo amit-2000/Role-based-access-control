e4276e75795f4c397ebd71050e33c5eeaf77ccab
amit-2000 <amitperane@gmail.com>
2024-01-07 19:59:15 +0530

    added controlled to authentication

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,7 @@
+
 const User = require("../models/User");
 const Role = require("../models/role");
 
-// Register a new user with a role
 exports.registerUser = (req, res) => {
   const { username, password, role } = req.body;
   const user = new User({ username, role });
@@ -13,4 +13,35 @@ exports.registerUser = (req, res) => {
     }
     res.json({ message: "User registered successfully" });
   });
+};
+
+// Login a user and create a session
+exports.loginUser = (req, res) => {
+  const { username, password } = req.body;
+
+  User.authenticate(username, password, (err, user) => {
+    if (err || !user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    // Create a new session and store user id
+    req.session.userId = user._id;
+
+    res.json({ message: "Login successful" });
+  });
+};
+
+// Logout a user and destroy the session
+exports.logoutUser = (req, res) => {
+  if (req.session) {
+    // Destroy the session
+    req.session.destroy((err) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      res.json({ message: "Logged out successfully" });
+    });
+  } else {
+    res.json({ message: "Not logged in" });
+  }
 };
